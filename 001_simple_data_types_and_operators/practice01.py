90bc572a5ab2ea667c03de5543fb003d984fb8ac
alxgurjev <86003517+alxgurjev@users.noreply.github.com>
2021-07-02 16:54:48 +0300

    number checker

---
+++ 001_simple_data_types_and_operators/practice01.py
@@ -0,0 +1,35 @@
+#task 1
+# user_data = ('name', 'surname', 'age')
+# name = 'Aleksandr'
+# surname = 'Gurjev'
+# age = 34
+# print(f'Hello {name} {surname}.Your age is {age}')
+
+#task 2
+# import math
+#
+# triangle = ('a', 'b', 'c')
+# a = 4
+# b = 5
+# c = math.sqrt(a ** 2 + b ** 2)
+# print(c)
+
+#task 3
+
+# triangle = input('a\n''b\n''c\n')
+# a, b, c = map(float, input(triangle).split())
+# a, b, c = sorted([a, b, c])
+#     if a * a + b * b == c * c:
+#         print('OK')
+#     else:
+#         print('NOT')
+
+#task 4
+numbers = input('Gimme numbers')
+print(str(numbers[-1]))
+
+#task 5
+
+
+
+
