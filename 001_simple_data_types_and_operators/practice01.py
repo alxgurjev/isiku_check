906cd3a4236b166d6601ce29ce5a2e0234fc1308
alxgurjev <86003517+alxgurjev@users.noreply.github.com>
2021-07-06 13:03:44 +0300

    number checker

--- 001_simple_data_types_and_operators/practice01.py
+++ 001_simple_data_types_and_operators/practice01.py
@@ -1,5 +1,5 @@
 #task 1
-# user_data = ('name', 'surname', 'age')
+
 # name = 'Aleksandr'
 # surname = 'Gurjev'
 # age = 34
@@ -19,17 +19,44 @@
 # triangle = input('a\n''b\n''c\n')
 # a, b, c = map(float, input(triangle).split())
 # a, b, c = sorted([a, b, c])
-#     if a * a + b * b == c * c:
+#     if a ** 2 + b ** 2 == c ** 2:
 #         print('OK')
 #     else:
 #         print('NOT')
 
 #task 4
-numbers = input('Gimme numbers')
-print(str(numbers[-1]))
 
+# message ='Gimme numbers'
+# index = 0
+# while index < len(message):
+#     index = index + 1
+#     print(message[-index])
 #task 5
 
+# var_a = 1, 2, 3, 5, 8
+# var_b = 8, 2, 5
+# var_c = f'{var_a}{var_b}'
+#
+# print(var_c)
+
+# task 6
+
+
+
+
+
+# task 7
+
+seconds = 1234565
+seconds_in_day = 60 * 60 * 24
+seconds_in_hour = 60 * 60
+seconds_in_minute = 60
+
+days = seconds // seconds_in_day
+hours = (seconds - (days * seconds_in_day)) // seconds_in_hour
+minutes = (seconds - (days * seconds_in_day) - (hours * seconds_in_hour)) // seconds_in_minute
+
+print(days, hours, minutes)
 
 
 
